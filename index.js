60d00ba3e13b2291f7166c15ba13344f1aff31e3
Jacqueline Tavarez <Jatavarez97@gmail.com>
2023-10-10 21:38:11 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Code your solutions in this file}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(namesarray, surprise){
+    const messages = []
+for (let i = 0; i < namesarray.length; i++){
+    console.log (`Thank you, ${namesarray[i]}, for the wonderful surprise gift!`);
+  messages.push(`Thank you, ${namesarray[i]}, for the wonderful surprise gift!`)
+
+}
+return messages
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "surprise");
+
+function countDown(startingnumber){
+    while (startingnumber > -1){
+console.log(startingnumber--);
+   }
+
+}
+
+countDown(10)
